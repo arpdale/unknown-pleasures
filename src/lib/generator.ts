b565346394b004aec632bdc509f95cd6705d5312
MentorUX <david.richard@gmail.com>
2025-11-20 15:05:52 -0600

    Fix: Add explicit width/height to SVG for mobile rendering

--- src/lib/generator.ts
+++ src/lib/generator.ts
@@ -134,7 +134,7 @@ export function generateJoyDivision(params: GeneratorParams): string {
         paths += `<path d="${linePath}" fill="none" stroke="black" stroke-width="${strokeWidth}" stroke-linecap="round" stroke-linejoin="round"/>\n`;
     }
 
-    return `<svg viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+    return `<svg width="100%" height="100%" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
         <rect width="${width}" height="${height}" fill="white"/>
         ${paths}
     </svg>`;
